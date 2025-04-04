623a528afa721b5012f71280b6672f681da38cc0
Matthew Elgert <mdelgert@yahoo.com>
2025-04-03 23:46:44 -0400

    Update platformio.ini and main.cpp to enable PSRAM initialization and logging enhancements

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,14 @@ void setup() {
   //Logger::init(); // Initialize the logger
   logger.init(); // Initialize the logger
 
+  if (psramInit()) {
+    Serial.println("✅ PSRAM initialized successfully!");
+    Serial.printf("PSRAM total: %u bytes\n", ESP.getPsramSize());
+    Serial.printf("PSRAM free : %u bytes\n", ESP.getFreePsram());
+    } else {
+      Serial.println("❌ PSRAM not found or failed to initialize");
+  }
+
   logI("Free heap memory: %d bytes", ESP.getFreeHeap());
   logI("Total heap memory: %d bytes", ESP.getHeapSize());
   logI("Free PSRAM memory: %d bytes", ESP.getFreePsram());
