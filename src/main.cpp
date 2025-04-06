938b5ae6076c79e25d50612a92b1bc94895ce4c4
Matthew Elgert <mdelgert@yahoo.com>
2025-04-05 20:33:01 -0400

    Refactor main loop and setup: comment out unused initializations and updates for clarity

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,18 @@ void setup()
   logger.init();
   settings.init();
   timer.init();
-  button.init();
-  led.init();
-  led.off();
-  tft.init();
+  // example.init();
+  // button.init();
+  // led.init();
+  // tft.init();
   
 }
 
 void loop()
 {
   timer.update();
-  button.update();
-  tft.update();
-  led.flash();
-  example.update();
+  // example.update();
+  // button.update();
+  // led.flash();
+  // tft.update();
 }
